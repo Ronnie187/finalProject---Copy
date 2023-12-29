be125dae9910843a42b63d89b2f71362f03c1155
JosephSingculan <133953808+JosephSingculan@users.noreply.github.com>
2023-12-29 21:42:29 +0800

    Create PackBase.js

---
+++ PackBase.js
@@ -0,0 +1,20 @@
+var imgs = ["0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg"];
+var packs = [
+    { name: "Pack 1", description: "Description for Pack 1",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
+    { name: "Pack 2", description: "Description for Pack 2",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}]  },
+    { name: "Pack 3", description: "Description for Pack 3" ,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 4", description: "Description for Pack 4",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
+    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 6", description: "Description for Pack 6",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] }
+];
+var keyword = "";
+
+
+
+module.exports = packs;
+module.exports = imgs;
+module.exports = keyword
