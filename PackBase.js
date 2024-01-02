ae31d36a1b7e3128bd6767a9a5c098f32b241c9d
JosephSingculan <133953808+JosephSingculan@users.noreply.github.com>
2024-01-02 10:32:48 +0800

    Update PackBase.js

--- PackBase.js
+++ PackBase.js
@@ -1,15 +1,24 @@
-var imgs = ["0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg","0921b93a7e54eaa1837cf1c1e1cd246e.jpg"];
+var imgs = ["osmena kawasan.png",
+            "badian oslob.png",
+            "sirao moalboal.png",
+            "museo oslob.png",
+            "tumalog mainit dive.png",
+            "mainit spring oslob sirao.png",
+            "bay bay moalboal busay cave.png",
+            "badian kawasan temple of leah.png",
+            "fort pedro taoist sumilon.png",
+            "cebu saffari kota beach omageica mangrove.png"];
 var packs = [
-    { name: "Pack 1", description: "Description for Pack 1",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
-    { name: "Pack 2", description: "Description for Pack 2",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}]  },
-    { name: "Pack 3", description: "Description for Pack 3" ,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 4", description: "Description for Pack 4",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
-    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 5", description: "Description for Pack 5" ,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 6", description: "Description for Pack 6",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] }
+    { name: "Pack 1", description: "Osmena Peak - Kawasan Falls",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:69000,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
+    { name: "Pack 2", description: "Badian Canyoneering - Oslob Whale Shark",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}]  },
+    { name: "Pack 3", description: "Sirao Garden - Moalboal Sardines",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 4", description: "Museo Oslob - Sumilon Sandbar",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
+    { name: "Pack 5", description: "Tumalog Falls- Mainit Dive",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 6", description: "Mainit Spring - Oslob Whale Shark - Sirao Garden",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 7", description: "Bay Bay beach - Moalboal Sardines - Busay Cave",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 8", description: "Badian Canyoneering - Temple of Leah - Kawasan",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 9", description: "Fort San Pedro - Taoist Temple - Sumilong Sandbar",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 10", description: "Cebu Saffari - Kota Beach - Omageica Mangrove",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] }
 ];
 var keyword = "";
 
